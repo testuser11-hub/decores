4790a1d21a66e597b156832914e1dcfcf0873fe0
testuser11-hub <mugeshkumartest@gmail.com>
2026-07-25 15:07:43 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        booking: resolve(__dirname, 'booking.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        props: resolve(__dirname, 'props.html'),
+        services: resolve(__dirname, 'services.html'),
+      },
+    },
+  },
+});
